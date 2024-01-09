229c4bfeac868d5eef1ac21169bfd42c8a0060ea
vdevigere <vdevigere+git@gmail.com>
2024-01-09 13:57:24 -0800

    Db Initialization scripts and sample data

---
+++ db-init-scripts/README.md
@@ -0,0 +1,3 @@
+# Script Initialization
+
+Add any initialization sql scripts here. This folder is mapped to /docker-entrypoint-initdb.d within compose.yaml
---
+++ db-init-scripts/sample_data.sql
@@ -0,0 +1,50 @@
+insert into flag
+values
+  (1, 'featureA'),
+  (2, 'featureB'),
+  (3, 'featureC'),
+  (4, 'featureD'),
+  (5, 'featureE'),
+  (6, 'featureF');
+insert into activation_config
+values
+  (
+    1, '{"startDateTime":"11-12-2023 12:00","endDateTime":"20-12-2023 12:00" }',
+    'DateTime', 'org.novi.activations.DateTimeActivationFactory'
+  ),
+  (
+    2, '{"SampleA":100.0,"SampleB":0,"SampleC":0}',
+    'Always SAMPLE A', 'org.novi.activations.WeightedRandomActivationFactory'
+  ),
+  (
+    3, '{"activationIds":[1,2],"operation":"AND"}',
+    '1 AND 2', 'org.novi.activations.factories.NoviOperationActivationFactory'
+  ),
+ (
+   4, '{"activationIds":[1,2],"operation":"OR"}',
+   '1 OR 2', 'org.novi.activations.factories.NoviOperationActivationFactory'
+ ),
+ (
+    5, '{"activationIds":[6,7],"operation":"AND"}',
+    '!False & (False | True)', 'org.novi.activations.factories.NoviOperationActivationFactory'
+  ),
+ (
+    6, '!org.novi.activations.dsl.FalseActivation("False-1") & (org.novi.activations.dsl.FalseActivation("False-2") | org.novi.activations.dsl.TrueActivation("True-3"))',
+    'DSL', 'org.novi.web.activations.DslEvaluator'
+ ),
+ (
+     7, 'org.novi.activations.dsl.FalseActivation("False-1") & (org.novi.activations.dsl.FalseActivation("False-2") | org.novi.activations.dsl.TrueActivation("True-3"))',
+     'DSL', 'org.novi.web.activations.DslEvaluator'
+  ),
+  (
+    8, 'does not matter', 'DSL activation used as non-dsl activation', 'org.novi.activations.dsl.DynamicActivation'
+  );
+insert into flag_activation_configs
+values
+  (1, 1),
+  (1, 2),
+  (2, 3),
+  (3, 4),
+  (4, 5),
+  (5, 7),
+  (6, 8);
