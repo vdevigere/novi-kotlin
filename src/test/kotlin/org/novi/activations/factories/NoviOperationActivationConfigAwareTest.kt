214aa14fa65558d075e24d615a884fe00c2e1bc7
vdevigere <vdevigere+git@gmail.com>
2024-01-10 14:33:26 -0800

    Moving AndActivationTest to its own test class

--- src/test/kotlin/org/novi/activations/factories/NoviOperationActivationConfigAwareTest.kt
+++ src/test/kotlin/org/novi/activations/factories/NoviOperationActivationConfigAwareTest.kt
@@ -110,39 +110,6 @@ class NoviOperationActivationConfigAwareTest {
         assertThat(activation.evaluate(context)).isFalse
     }
 
-    @Test
-    fun testAndActivationFactory() {
-        val op1config = ActivationConfig(
-            config = "{\"startDateTime\":\"11-12-2023 12:00\",\"endDateTime\":\"20-12-2023 12:00\" }",
-            description = "Date Time",
-            name = "org.novi.activations.DateTimeActivationFactory",
-            id = 1L
-        )
-        val op2config = ActivationConfig(
-            config = "{\"SampleA\":100.0,\"SampleB\":0,\"SampleC\":0}",
-            description = "Always Sample A",
-            name = "org.novi.activations.WeightedRandomActivationFactory",
-            id = 2L
-        )
-
-        val mockRepo = mock(ActivationConfigRepository::class.java)
-        `when`(mockRepo.findById(1L)).thenReturn(Optional.of(op1config))
-        `when`(mockRepo.findById(2L)).thenReturn(Optional.of(op2config))
-        `when`(mockRepo.findAllById(listOf(1L, 2L))).thenReturn(listOf(op1config, op2config))
-        val context = """
-                {
-                    "org.novi.activations.DateTimeActivation.currentDateTime": "15-12-2023 12:00",
-                    "org.novi.activations.WeightedRandomActivation":{
-                                            "seed": 200,
-                                            "variantToCheck": "SampleB"
-                                        }                    
-                }
-                """
-        val factory = AndActivationFactory()
-        val activation = factory.setActivationConfigRepository(mockRepo).setConfiguration("[1,2]")
-        assertThat(activation.evaluate(context)).isFalse
-    }
-
     companion object {
         @JvmStatic
         @BeforeAll
