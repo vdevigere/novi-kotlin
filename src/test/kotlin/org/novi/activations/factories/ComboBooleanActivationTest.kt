4c4cd3ae5532f41e9a519c8d13ce2a3b4e5d9e29
vdevigere <vdevigere+git@gmail.com>
2024-01-10 14:38:38 -0800

    Adding tests for ComboBooleanActivation

---
+++ src/test/kotlin/org/novi/activations/factories/ComboBooleanActivationTest.kt
@@ -0,0 +1,97 @@
+package org.novi.activations.factories
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.mockito.Mockito
+import org.novi.NoviConfiguration
+import org.novi.persistence.ActivationConfig
+import org.novi.persistence.ActivationConfigRepository
+import java.util.*
+
+class ComboBooleanActivationTest {
+
+    @Test
+    fun testAnd() {
+        val op1config = ActivationConfig(
+            config = "{\"startDateTime\":\"11-12-2023 12:00\",\"endDateTime\":\"20-12-2023 12:00\" }",
+            description = "Date Time",
+            name = "org.novi.activations.DateTimeActivationFactory",
+            id = 1L
+        )
+        val op2config = ActivationConfig(
+            config = "{\"SampleA\":100.0,\"SampleB\":0,\"SampleC\":0}",
+            description = "Always Sample A",
+            name = "org.novi.activations.WeightedRandomActivationFactory",
+            id = 2L
+        )
+
+        val mockRepo = Mockito.mock(ActivationConfigRepository::class.java)
+        Mockito.`when`(mockRepo.findById(1L)).thenReturn(Optional.of(op1config))
+        Mockito.`when`(mockRepo.findById(2L)).thenReturn(Optional.of(op2config))
+        Mockito.`when`(mockRepo.findAllById(listOf(1L, 2L))).thenReturn(listOf(op1config, op2config))
+
+        val context = """
+                {
+                    "org.novi.activations.DateTimeActivation.currentDateTime": "15-12-2023 12:00",
+                    "org.novi.activations.WeightedRandomActivation":{
+                                            "seed": 200,
+                                            "variantToCheck": "SampleB"
+                                        }                    
+                }
+                """
+        val config = """
+            {"activationIds":[1,2],"operation":"AND"}
+        """.trimIndent()
+        val factory = ComboBooleanActivationFactory()
+        val activation = factory.setActivationConfigRepository(mockRepo).setConfiguration(config)
+        Assertions.assertThat(activation.evaluate(context)).isFalse
+    }
+
+    @Test
+    fun testOr() {
+        val op1config = ActivationConfig(
+            config = "{\"startDateTime\":\"11-12-2023 12:00\",\"endDateTime\":\"20-12-2023 12:00\" }",
+            description = "Date Time",
+            name = "org.novi.activations.DateTimeActivationFactory",
+            id = 1L
+        )
+        val op2config = ActivationConfig(
+            config = "{\"SampleA\":100.0,\"SampleB\":0,\"SampleC\":0}",
+            description = "Always Sample A",
+            name = "org.novi.activations.WeightedRandomActivationFactory",
+            id = 2L
+        )
+
+        val mockRepo = Mockito.mock(ActivationConfigRepository::class.java)
+        Mockito.`when`(mockRepo.findById(1L)).thenReturn(Optional.of(op1config))
+        Mockito.`when`(mockRepo.findById(2L)).thenReturn(Optional.of(op2config))
+        Mockito.`when`(mockRepo.findAllById(listOf(1L, 2L))).thenReturn(listOf(op1config, op2config))
+
+        val context = """
+                {
+                    "org.novi.activations.DateTimeActivation.currentDateTime": "15-12-2023 12:00",
+                    "org.novi.activations.WeightedRandomActivation":{
+                                            "seed": 200,
+                                            "variantToCheck": "SampleA"
+                                        }                    
+                }
+                """
+        val config = """
+            {"activationIds":[1,2],"operation":"OR"}
+        """.trimIndent()
+        val factory = ComboBooleanActivationFactory()
+        val activation = factory.setActivationConfigRepository(mockRepo).setConfiguration(config)
+        Assertions.assertThat(activation.evaluate(context)).isTrue
+    }
+
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun initializeRegistry(): Unit {
+            NoviConfiguration()
+        }
+    }
+}
