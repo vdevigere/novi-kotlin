3b867b6734ed0493386b213ee4fe3cbc9d0b32b4
vdevigere <vdevigere+git@gmail.com>
2024-01-13 12:25:17 -0800

    Support for registering plugins from a plugins folder.

--- src/main/kotlin/org/novi/NoviConfiguration.kt
+++ src/main/kotlin/org/novi/NoviConfiguration.kt
@@ -1,22 +1,34 @@
 package org.novi
 
 import org.novi.core.ActivationConfigAware
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import java.io.File
+import java.net.URL
+import java.net.URLClassLoader
 import java.util.*
 import kotlin.reflect.KClass
 
+
 @Configuration
-class NoviConfiguration() {
+class NoviConfiguration(@Value("\${activations.plugin.dir}") final val plugin_dir: String = "./plugin_activations") {
 
     init {
-        registerPlugins()
+        val pluginDir = File(plugin_dir)
+        val fList = pluginDir.listFiles { file -> file.path.lowercase().endsWith(".jar") }
+        val urls = fList?.map { file -> file.toURI().toURL() }?.toTypedArray()
+        registerPlugins(urls)
+
     }
 
-    private final fun registerPlugins() {
-        val loader = ServiceLoader.load(ActivationConfigAware::class.java)
-        for (factory in loader) {
-            REGISTRY.instance[factory::class] = factory
+    private final fun registerPlugins(urls: Array<URL>?) {
+        var loader = ServiceLoader.load(ActivationConfigAware::class.java)
+        if(urls!=null){
+            loader = ServiceLoader.load(ActivationConfigAware::class.java, URLClassLoader.newInstance(urls, NoviConfiguration::class.java.classLoader))
         }
+        for (factory in loader) {
+                REGISTRY.instance[factory::class] = factory
+            }
     }
 }
 
