885c9ecfcaac32b39a5c74488a15be470f3824ab
vdevigere <vdevigere+git@gmail.com>
2024-01-19 10:34:08 -0800

    Initialize data via JPA instead of Postgres specific SQL.

--- src/main/kotlin/org/novi/NoviConfiguration.kt
+++ src/main/kotlin/org/novi/NoviConfiguration.kt
@@ -1,7 +1,15 @@
 package org.novi
 
 import org.novi.core.ActivationConfigAware
+import org.novi.persistence.ActivationConfig
+import org.novi.persistence.ActivationConfigRepository
+import org.novi.persistence.Flag
+import org.novi.persistence.FlagRepository
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.File
 import java.net.URL
@@ -11,7 +19,8 @@ import kotlin.reflect.KClass
 
 
 @Configuration
-class NoviConfiguration(@Value("\${activations.plugin.dir}") final val plugin_dir: String = "./plugin_activations") {
+class NoviConfiguration(@Value("\${activations.plugin.dir}") val plugin_dir: String = "./plugin_activations") {
+    private val logger = LoggerFactory.getLogger(NoviConfiguration::class.java)
 
     init {
         val pluginDir = File(plugin_dir)
@@ -31,6 +40,50 @@ class NoviConfiguration(@Value("\${activations.plugin.dir}") final val plugin_di
         }
         for (factory in loader) {
             REGISTRY.instance[factory::class] = factory
+        }
+    }
+
+    @Bean
+    fun initializeDb(
+        @Autowired flagRepository: FlagRepository,
+        @Autowired activationConfigRepository: ActivationConfigRepository
+    ): CommandLineRunner {
+        logger.debug("Initializing Db....")
+        return CommandLineRunner {
+            val ac1 = ActivationConfig(
+                1L, "org.novi.activations.DateTimeActivationFactory",
+                "DateTime", """{"startDateTime":"11-12-2023 12:00","endDateTime":"20-12-2023 12:00" }"""
+            )
+            val ac2 = ActivationConfig(
+                2L, "org.novi.activations.WeightedRandomActivationFactory",
+                "Always SAMPLE A", """{"SampleA":100.0,"SampleB":0,"SampleC":0}""",
+            )
+            val ac3 = ActivationConfig(
+                3L, "org.novi.activations.ComboBooleanActivationFactory",
+                "1 AND 2", """{"activationIds":[1,2],"operation":"AND"}""",
+            )
+            val ac4 = ActivationConfig(
+                4L, "org.novi.activations.ComboBooleanActivationFactory",
+                "1 OR 2", """{"activationIds":[1,2],"operation":"OR"}""",
+            )
+            val ac5 = ActivationConfig(
+                5L, "org.novi.core.AndActivationFactory",
+                "DateTimeActivation && WeightedRandomActivation", "[1,2]"
+            )
+            val ac6 = ActivationConfig(
+                6L, "org.novi.core.OrActivationFactory",
+                "DateTimeActivation && WeightedRandomActivation", "[1,2]",
+            )
+
+            val flag1 = Flag(1L, "Implicit AND ids 1, 2", false, setOf(ac1, ac2))
+            val flag2 = Flag(2L, "Use BooleanActivFac to AND 1, 2", false, setOf(ac3))
+            val flag3 = Flag(3L, "Use BooleanActivFac to OR 1, 2", false, setOf(ac4))
+            val flag4 = Flag(4L, "Use AndActivFac to AND 1, 2", false, setOf(ac5))
+            val flag5 = Flag(5L, "Use OrActivFac to OR 1, 2", false, setOf(ac6))
+            val flag6 = Flag(6L, "featureF", false, Collections.emptySet())
+
+            activationConfigRepository.saveAll(listOf(ac1, ac2, ac3, ac4, ac5, ac6))
+            flagRepository.saveAll(listOf(flag1, flag2, flag3, flag4, flag5, flag6))
         }
     }
 }
